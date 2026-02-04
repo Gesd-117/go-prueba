34d87513881fbe394bf040293801adbc3af5ac12
Gesd-117 <gesd117@gmail.com>
2026-02-04 10:49:42 -0400

    mensaje

--- lista/lista.go
+++ lista/lista.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
 	util "arsistema/utilidades"
-	_"strings"
-	_"slices"
+	"fmt"
+	"slices"
+	_ "strings"
 )
-
 
 func main() {
 	edades := []int{
@@ -22,20 +21,71 @@ func main() {
 		17,
 		30,
 	}
-	
-	util.Linea(50)
-	fmt.Println("la cantidad de edades es", len(edades))
-	util.Linea(50)
-	fmt.Println("la tercera edad es", edades[2])
-	util.Linea(50)
-	fmt.Println("la quinta edad es", edades[4])
-	util.Linea(50)
-	fmt.Println("la ultima edad es", edades[len(edades)-1])
-	util.Linea(50)
-
 	edades = append(edades, 30)
 	edades = append(edades, 33)
+
+	util.Encabezado("Leer la lista de edades", 50)
+	listaDeEdades(edades...)
+
+	listaDeEdadesAsc(edades...)
+	listaDeEdadesDes(edades...)
+	edadMaxMin(edades...)
+}
+
+func listaDeEdades(edades ...int) {
+	for indice, edad := range edades {
+		fmt.Printf("La edad numero %v es: %v\n", indice+1, edad)
+	}
+}
+
+func listaDeEdadesAsc(edades ...int) {
+	util.Encabezado("lista de edades ordenada ascendentemente", 70)
+	slices.Sort(edades)
+	listaDeEdades(edades...)
+}
+
+func listaDeEdadesDes(edades ...int) {
+	util.Encabezado("lista de edades ordenada descendentemente", 70)
+	slices.SortFunc(edades, func(a, b int) int {
+		return b - a
+	})
+	listaDeEdades(edades...)
+}
+
+func edadMaxMin(edades ...int) {
+
+	util.Encabezado("Edad mas maxima y minima", 70)
+
+	var (
+		edadMinima int
+		edadMaxima int
+	)
+
+	for indice, edad := range edades {
+
+		if indice == 0 {
+			edadMaxima = edad
+			edadMinima = edad
+		}
+
+		if edadMaxima < edad {
+			edadMaxima = edad
+		}
+		if edadMinima > edad {
+			edadMinima = edad
+		}
+
+	}
+
+	fmt.Printf("La edad maxima  es: %v\n", edadMaxima)
+	fmt.Printf("La edad minima es: %v\n", edadMinima)
+	util.Linea(70)
+
+}
+
+func listaMayoresDeEdad(edades ...int)  {
 	
-	fmt.Println("la ultima edad es", edades[len(edades)-1])
-	util.Linea(50)
+	util.Encabezado("lista de mayores de edad", 50)
+	listaDeEdades(edades...)
+	
 }
